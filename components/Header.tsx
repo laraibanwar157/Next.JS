b086d37cacea7a10c598871f7572a4cfb1650f69
Laraib Anwar <laraibanwar157@gmail.com>
2024-11-10 23:17:56 +0500

    code updated

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="bg-[#A29875] h-24">
+          <h2 className="text-[#FFFFFF] text-[75px] font-serif ml-10">MANZZARI</h2>
+        </div>
+    );
+};
+
+export default Header;
